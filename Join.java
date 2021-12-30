4507ea920e45388d4f14d9a66c2e147f14e80a61
TeluguSravani <telugusravani2@gmail.com>
2021-12-30 18:45:29 +0530

    third commit

---
+++ Join.java
@@ -0,0 +1,6 @@
+public class Join {
+    public static void main(String[] args){
+        String take = String.join("*", "telugu","sravani","will get","a","government job");
+        System.out.println(take);
+    }
+}
