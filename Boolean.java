dd4fabf90d8481b773fe73f30078f6bc5f52e334
TeluguSravani <telugusravani2@gmail.com>
2021-12-30 18:34:52 +0530

    this program is simple to code

---
+++ Boolean.java
@@ -0,0 +1,7 @@
+public class Boolean{
+    public static void main(String[] args){
+        String git = "my mom is good looking";
+        System.out.println(git.contains("mom is "));
+        System.out.println(git.contains("my is good"));
+    }
+}
