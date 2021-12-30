db07779c01b2b2be9b6b69166fee0c53799a7694
TeluguSravani <telugusravani2@gmail.com>
2021-12-30 18:44:08 +0530

    first commit

---
+++ Day12.java
@@ -0,0 +1,9 @@
+public class Day12{
+public static void main(String[] args){
+String fit = "he is a very very good boy , is n't he ";
+String[] words = fit.split("\\s");
+for(String w:words){
+System.out.println(w);
+}
+}
+} 
