5d64141796fb4073a81e546269b652c4d1af024a
TeluguSravani <telugusravani2@gmail.com>
2022-01-05 17:57:38 +0530

    first commit

---
+++ Day11.java
@@ -0,0 +1,8 @@
+public class Day11 {
+    public static void main(String[] args){
+        String str = "this college name is ASKW";
+       int index1 = str.indexOf("this");
+       int index2 = str.indexOf("ASKW");
+       System.out.println(index1+ " " +index2);
+    }
+}
