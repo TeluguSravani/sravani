0be2b87d7f45e60d780c96d5836248e55f22e0f6
TeluguSravani <telugusravani2@gmail.com>
2022-01-05 18:02:51 +0530

    second commit

---
+++ Equals.java
@@ -0,0 +1,7 @@
+public class Equals {
+    public static void main(String[] args){
+        String die = "my name is sravani";
+        String fluct = "my good name is telugu";
+        System.out.println(die.equals(fluct));
+    }
+}
